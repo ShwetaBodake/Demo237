7a208c1f3b34d93376d90459ee0491fcb8790e2b
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-19 22:35:11 +0530

    Main Method

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /*Console.WriteLine("Hello World");
+            int val = 0;
+            double val2= 4.7;
+            Boolean Xyz = true;
+            char val4 = 'f';
+
+            Console.WriteLine("Integer Value is" + val);
+            Console.WriteLine("Double is " +val2 );
+            Console.WriteLine("Boolean value is " +Xyz);
+            Console.WriteLine("Character is " + val4);
+
+           
+            Console.WriteLine("The name of user ");
+            string x =Console.ReadLine();
+
+            Console.WriteLine("Enter the first number ");
+            int number1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number ");
+            int number2=Convert.ToInt32(Console.ReadLine());
+
+            int addition = number1 + number2;
+            Console.WriteLine("The summation is " + addition);
+            //Console.ReadLine();
+
+            Console.WriteLine("First Number is");
+            int First = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second Number is ");
+            int Second = Convert.ToInt32(Console.ReadLine());
+
+            int Substraction = First - Second;
+            Console.WriteLine("The Substraction is " + Substraction);
+            */
+
+            //VowelOrConstant.CheckCharacter();
+           //CheckCharacter LargestNumbers.DisplayLargestNumbers();
+           //EvenOddProgram.FindEvenOddProgram();
+           // SwapNumbers.DisplaySwapNumbers();
+           //  Quotient_Reminder.find_Quotient_Reminder();
+           //HormonicNumber.findHormonicNumber();
+           //PowerOfTwo.findPowerOfTwo();
+           //LeapYear.isLeapYear();
+           FlipCoin.FlipTheCoin();
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Please enter the username ");
+            //string username = Console.ReadLine();
+            //int num = username.Length;
+            //Console.WriteLine(num);
+
+            //if (num < 3)
+            //{
+            //    Console.WriteLine("The Username length character is Smaller");
+            //}
+            //else if(num==3)
+            //    {
+            //    Console.WriteLine("Username having 3 characters");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hi " + username + "good morning");
+            //}
+        }
+    }
+}
+
