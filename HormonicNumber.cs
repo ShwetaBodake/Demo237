6bcbe285456def8b30f28f8f88f3cbfeb0e0a3b8
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-19 22:54:22 +0530

    Program of Hormonic Series

---
+++ HormonicNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HelloWorld
+{
+	public class HormonicNumber
+	{
+		public static void findHormonicNumber()
+		{
+			double sum = 0.0;
+
+			Console.WriteLine("Enter Hormonic Value ");
+			int Hormonic_Number = Convert.ToInt32(Console.ReadLine());
+
+			for (double i = 1; i <= Hormonic_Number; i++)
+			{
+				//Console.WriteLine("1/" + i + "+ ");
+				sum = sum + (1 / i);
+			}
+			Console.WriteLine("The sum is "+sum);
+		}
+	}
+}
+
